185c52eaa9f1d0f956e4e9b20a1d5943090be142
Jaimin <jemiisidd66@gmail.com>
2021-02-06 00:54:07 +0530

    ongoing

---
+++ calsi.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.get("/", function(req, res){
+res.sendFile(__dirname + "/calsi.html");
+});
+
+app.post("/", function(req, res){
+    
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 * num2 ;
+
+    res.send("Result" +result);
+
+})
+
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/bmicalsi.html");
+    });
+    app.post("/", function(req, res){
+    
+        var weight = parseFloat(req.body.weight);
+        var height = parseFloat(req.body.height);
+    
+        var bmi = weight / (height * height) ;
+    
+        res.send("BMI is" +bmi);
+    
+    })
+    
+
+
+
+app.listen(3000, function(){
+    console.log("Server for 3000");
+});
